2fa1ee027cbd7aa6623cafd9c2ce1588d4b21fd1
Derek Parker <deparker@redhat.com>
2018-04-23 15:10:00 -0700

    crypto/boring: Port rest of RSA functionality

--- src/crypto/internal/boring/openssl_port_rsa.c
+++ src/crypto/internal/boring/openssl_port_rsa.c
@@ -71,3 +71,116 @@ int
   BN_free(e);
   return ret;
 }
+
+// Only in BoringSSL.
+int _goboringcrypto_RSA_sign_raw(GO_RSA *rsa, size_t *out_len, uint8_t *out, size_t max_out,
+                 const uint8_t *in, size_t in_len, int padding) {
+  // NOTE(deparker) BoringSSL RSA interface includes the method `rsa->meth->sign_raw`. This is
+  // not implemented in OpenSSL, nor is it used in the Go caller. See {BoringSSL}/crypto/fipsmodule/rsa/rsa.c:283
+  // for an example usage.
+
+  return rsa_default_sign_raw(rsa, out_len, out, max_out, in, in_len, padding);
+}
+
+// Only in BoringSSL.
+int rsa_default_sign_raw(GO_RSA *rsa, size_t *out_len, uint8_t *out,
+                         size_t max_out, const uint8_t *in, size_t in_len,
+                         int padding) {
+	EVP_PKEY *pkey = EVP_PKEY_new();
+	EVP_PKEY_assign_RSA(pkey, rsa);
+        EVP_PKEY_CTX *ctx;
+	size_t siglen;
+	int ret;
+
+        ctx = EVP_PKEY_CTX_new(pkey, NULL /* no engine */);
+        if (!ctx) {
+		ret = 0;
+		goto err;
+	}
+        if (EVP_PKEY_sign_init(ctx) <= 0) {
+		ret = 0;
+		goto err;
+	}
+
+	switch(padding) {
+	// No padding is acceptable in this sign raw function because it is
+	// called from other functions that may have already set the padding.
+	case GO_RSA_NO_PADDING:
+	case GO_RSA_PKCS1_PADDING:
+		if (EVP_PKEY_CTX_set_rsa_padding(ctx, padding) <= 0) {
+			ret = 0;
+			goto err;
+		}
+		break;
+	default:
+		ret = 0;
+		goto err;
+	}
+
+        /* Determine buffer length */
+        if (EVP_PKEY_sign(ctx, NULL, &siglen, in, in_len) <= 0) {
+		ret = 0;
+		goto err;
+	}
+	if (max_out < siglen) {
+		ret = 0;
+		goto err;
+	}
+
+        if (EVP_PKEY_sign(ctx, out, out_len, in, in_len) <= 0)
+		ret = 0;
+
+err:
+	EVP_PKEY_CTX_free(ctx);
+	return ret;
+}
+
+int _goboringcrypto_RSA_sign_pss_mgf1(RSA *rsa, size_t *out_len, uint8_t *out, size_t max_out,
+                      const uint8_t *in, size_t in_len, const EVP_MD *md,
+                      const EVP_MD *mgf1_md, int salt_len) {
+  if (in_len != EVP_MD_size(md)) {
+    return 0;
+  }
+
+  size_t padded_len = RSA_size(rsa);
+  uint8_t *padded = OPENSSL_malloc(padded_len);
+  if (padded == NULL) {
+    return 0;
+  }
+
+  int ret =
+      RSA_padding_add_PKCS1_PSS_mgf1(rsa, padded, in, md, mgf1_md, salt_len) &&
+      _goboringcrypto_RSA_sign_raw(rsa, out_len, out, max_out, padded, padded_len,
+                   RSA_NO_PADDING);
+  OPENSSL_free(padded);
+  return ret;
+}
+
+int _goboringcrypto_RSA_verify_pss_mgf1(RSA *rsa, const uint8_t *msg, size_t msg_len,
+                        const EVP_MD *md, const EVP_MD *mgf1_md, int salt_len,
+                        const uint8_t *sig, size_t sig_len) {
+  if (msg_len != EVP_MD_size(md)) {
+    return 0;
+  }
+
+  size_t em_len = RSA_size(rsa);
+  uint8_t *em = OPENSSL_malloc(em_len);
+  if (em == NULL) {
+    return 0;
+  }
+
+  int ret = 0;
+  if (!_goboringcrypto_RSA_verify_raw(rsa, &em_len, em, em_len, sig, sig_len, RSA_NO_PADDING)) {
+    goto err;
+  }
+
+  if (em_len != RSA_size(rsa)) {
+    goto err;
+  }
+
+  ret = RSA_verify_PKCS1_PSS_mgf1(rsa, msg, md, mgf1_md, em, salt_len);
+
+err:
+  OPENSSL_free(em);
+  return ret;
+}
